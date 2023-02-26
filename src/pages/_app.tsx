84f65e011ab0a555fe1605811b03c0187a9cd396
shaun <modipanesh@gmail.com>
2023-02-26 18:44:49 +0200

    initialising the chakra UI and Sessession

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,21 @@
 import type { AppProps } from 'next/app';
+import { theme } from '../../lib/chakra/theme';
 import { ApolloProvider } from '@apollo/client';
+import { SessionProvider } from 'next-auth/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import apolloClient from '../../lib/graphQL/apolloClient';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+	Component,
+	pageProps: { session, ...pageProps },
+}: AppProps) {
 	return (
-		<ApolloProvider client={apolloClient}>
-			<Component {...pageProps} />
-		</ApolloProvider>
+		<SessionProvider session={session}>
+			<ChakraProvider theme={theme}>
+				<ApolloProvider client={apolloClient}>
+					<Component {...pageProps} />
+				</ApolloProvider>
+			</ChakraProvider>
+		</SessionProvider>
 	);
 }
