96b133cc9606df4d55bae42b3378a106303a5c50
shaun <modipanesh@gmail.com>
2023-02-26 22:44:32 +0200

    added structure & styling to the Auth component

---
+++ components/Auth/CreateUsername/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const CreateUsername = (props: Props) => {
+  return (
+    <div>CreateUsername</div>
+  )
+}
+
+export default CreateUsername
--- components/Auth/index.tsx
+++ components/Auth/index.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Center, Stack } from '@chakra-ui/react';
+import type { Session } from 'next-auth';
+import { useSession } from 'next-auth/react';
+import CreateUsername from './CreateUsername';
+import SignIn from './SignIn';
 
-type Props = {}
+type Props = {
+	session: Session;
+};
 
-const Auth = (props: Props) => {
-  return (
-    <div>Auth</div>
-  )
-}
+const Auth = () => {
+	const { data: session } = useSession();
+	return (
+		<Center height="100vh">{session ? <CreateUsername /> : <SignIn />}</Center>
+	);
+}; //this component is responsible for authentication our user then allow them to create their username
 
-export default Auth
+export default Auth;
