46358c1af373463718702a87848e5b64e55964fd
shaun <modipanesh@gmail.com>
2023-02-26 22:53:02 +0200

    added state and styling to CreateUsername component

--- components/Auth/CreateUsername/index.tsx
+++ components/Auth/CreateUsername/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Stack, Text, Input, Button } from '@chakra-ui/react';
 
-type Props = {}
+type Props = {};
 
 const CreateUsername = (props: Props) => {
-  return (
-    <div>CreateUsername</div>
-  )
-}
+	const [username, setUsername] = useState('');
 
-export default CreateUsername
+	return (
+		<Stack spacing="8">
+			<Text fontSize="3xl">create your username</Text>
+			<Input
+				placeholder="enter user name"
+				type="text"
+				value={username} //look here
+				onChange={event => setUsername(event.target.value)} //look here
+			/>
+			<Button width="100%">Submit</Button>
+		</Stack>
+	);
+};
+
+export default CreateUsername;
