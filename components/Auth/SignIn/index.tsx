2fb862e0e05331efac863179d11b35f99d1bd020
shaun <modipanesh@gmail.com>
2023-02-26 22:48:18 +0200

    Styled SignIn component added sign-in functionality

--- components/Auth/SignIn/index.tsx
+++ components/Auth/SignIn/index.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Stack, Text, Button, Image } from '@chakra-ui/react';
+import { signIn } from 'next-auth/react';
 
-type Props = {}
+type Props = {};
 
 const SignIn = (props: Props) => {
-  return (
-    <div>SignIn</div>
-  )
-}
+	return (
+		<Stack align="center" spacing="5">
+			<Text fontSize="3xl">mychatapp</Text>
+			<Button
+				onClick={() => signIn('google')}
+				leftIcon={<Image src="./google.svg" alt="google-logo" height="25px" />}>
+				please sign in with google
+			</Button>
+		</Stack>
+	);
+};
 
-export default SignIn
+export default SignIn;
