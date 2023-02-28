544be6241dc6e47e133f0af2479a2765077ae3b7
shaun <modipanesh@gmail.com>
2023-02-28 17:21:12 +0200

    set-up apolloclient to send websocket connections

--- src/lib/graphQL/apolloClient.ts
+++ src/lib/graphQL/apolloClient.ts
@@ -1,12 +1,39 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { createClient } from 'graphql-ws';
 
 const httpLink = new HttpLink({
 	uri: 'http://localhost:4000/graphql', //here is the endpoints to our graphql server
 	credentials: 'include', //this prop will allow the client to send sessions to the client
 }); //this object will create http requests to our server so that we can send query/mutation request
 
+const wsLink =
+	typeof window !== 'undefined'
+		? new GraphQLWsLink(
+				createClient({
+					url: 'ws://localhost:4000/graphql/subscriptions',
+				}),
+		  )
+		: null;
+
+const link =
+	typeof window !== 'undefined' && wsLink !== null
+		? split(
+				({ query }) => {
+					const definition = getMainDefinition(query);
+					return (
+						definition.kind === 'OperationDefinition' &&
+						definition.operation === 'subscription'
+					);
+				},
+				wsLink,
+				httpLink,
+		  )
+		: httpLink;
+
 const apolloClient = new ApolloClient({
-	link: httpLink,
+	link,
 	connectToDevTools: process.env.NODE_ENV !== 'production',
 	queryDeduplication: true,
 	cache: new InMemoryCache(), //this prop will cacche our response so that we it wont perform unnecessary requests
