73c1db2fd8403f6e6efeffc9049520da6060b2e6
shaun <modipanesh@gmail.com>
2023-03-03 11:14:31 +0200

    ready to send message

--- src/components/Chat/Feed/Messages/Input/index.tsx
+++ src/components/Chat/Feed/Messages/Input/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Input } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import toast from 'react-hot-toast';
 
 type Props = {
 	conversationId: string;
@@ -7,21 +8,32 @@ type Props = {
 
 const MessageInput = ({ conversationId }: Props) => {
 	const [messageBody, setMessageBody] = useState('');
+	const sendMessage = async (event: React.FormEvent) => {
+		event.preventDefault();
+		try {
+			/**
+			 * send the message
+			 */
+		} catch (error) {
+			console.error("sendMessage error: ", error);
+			toast.error('failed to send message');
+		}
+	};
 	return (
 		<Box px="4" py="6" width="100%">
-			<form onSubmit={() => {}}>
+			<form onSubmit={event => sendMessage(event)}>
 				<Input
-                    type="text"
-                    value={messageBody}
-                    onChange={event => setMessageBody(event.target.value)}
-                    size="md"
-                    resize="none"
-                    placeholder='enter message'
-                    _focus={{
-                        boxShadow: 'none',
-                        border: '1px solid',
-                        borderColor: 'whiteAlpha.300',
-                    }}
+					type="text"
+					value={messageBody}
+					onChange={event => setMessageBody(event.target.value)}
+					size="md"
+					resize="none"
+					placeholder="enter message"
+					_focus={{
+						boxShadow: 'none',
+						border: '1px solid',
+						borderColor: 'whiteAlpha.300',
+					}}
 				/>
 			</form>
 		</Box>
